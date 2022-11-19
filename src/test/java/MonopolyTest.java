1fc92499b2f2094e3df3e518b79232de0e0d051a
sandy2795 <sandeeparumugam3@gmail.com>
2022-11-19 16:43:32 +0530

    Test cases updated

--- src/test/java/MonopolyTest.java
+++ src/test/java/MonopolyTest.java
@@ -1,11 +1,7 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MonopolyTest {
     Monopoly player = new Monopoly(0);
@@ -13,10 +9,17 @@ class MonopolyTest {
     @Test
     public void when_die_rolls_are_1_and_2_current_position_should_advance_to_3(){
         //WRITE CODE HERE
+        Monopoly spyPlayer = Mockito.spy(player);
+        Mockito.when(spyPlayer.throwDie()).thenReturn(1,2);
+        assertEquals(3,spyPlayer.advance());
     }
     @Test
     public void when_die_rolls_are_3_and_3_advance_should_be_called_again_and_when_1_and_3_are_rolled_current_position_should_be_updated_to_10(){
         // WRITE CODE HERE
+        Monopoly spyPlayer = Mockito.spy(player);
+        Mockito.when(spyPlayer.throwDie()).thenReturn(3,3);
+        assertEquals(9,spyPlayer.advance());
+
     }
 
 }
